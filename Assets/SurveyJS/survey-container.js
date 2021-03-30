beadeecfbf1dce0bee75261ceb4fff8c70fde3e5
Venkat <venkat.b@thoughtgreen.com>
2021-03-30 14:08:03 +0530

    Added custom ui for wellbeing survey and group allocation survey.

--- Assets/SurveyJS/survey-container.js
+++ Assets/SurveyJS/survey-container.js
@@ -36,6 +36,15 @@ function loadSurvey(json) {
 
   // Add survey to the global context for access
   window.survey = survey;
+
+  if (survey.firstPageIsStarted || survey.isLastPage) {
+    // If the first page is a starter page, or is the last page, then hide the progress navigation
+    hideNavigation();
+  }
+  else{
+    showNavigation();
+  }
+  
 }
 
 function surveyStarted(survey) {
@@ -82,6 +91,7 @@ function onCurrentPageChanged(survey) {
 
   // Send message to context that page has changed
   EmbedContext.sendMessage("pageChanged", { page: survey.currentPageNo });
+  navigationUiApply(survey);
 }
 
 function hideNavigation() {
@@ -134,12 +144,22 @@ function setCustomProperties(survey, jsonObj) {
  * Apply navigation property changes to the survey layout.
  * 
  * @param survey - Survey object
+ * 
  */
 function navigationUiApply(survey) {
-  if(survey.getPropertyValue("questionNavigationUiType") == "NO_PROGRESS_BAR"){
+  var customUI = survey.getPropertyValue("questionNavigationUiType");
+
+  if(customUI == "NO_PROGRESS_BAR") {
     $("#surveyProgress").hide();
     $(".pagination").addClass("no_progress_bar");
-    $(".panel-footer").addClass("no_progress_footer_position");
+  }
+
+  else if(customUI == "WELL_BEING") {
+    $(".form-control").addClass("well_being_form_control");
+  }
+
+  else if(customUI == "CEQ") {
+    $(".btn-group > .btn").addClass("ceq_survey_label");
   }
 }
 
